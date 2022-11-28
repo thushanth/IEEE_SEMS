d5bb89e0ca049b6e375b3310f471300c895a9bf2
Thushanth <thushanth.sithambararajan@mail.concordia.ca>
2022-11-28 18:21:42 -0500

    Update PiZero.py
    Modified, Raspberry Pi Py code is time based, fire and forget.

--- PiZero.py
+++ PiZero.py
@@ -1,8 +1,8 @@
 #------------------------
 #       Libraries
 #------------------------
-
-
+import time
+import RPi.GPIO as GPIO
 
 #------------------------
 #       Variables
@@ -13,14 +13,25 @@
 #------------------------
 #       Pin Setup
 #------------------------
-
+GPIO.setmode(GPIO.BCM)
 
 
 #------------------------
-#       Functions
+#       Aux. Functions
 #------------------------
+def doorStatus():
+    return null
 
+def postToDB():
+    return null
+
+
+#------------------------
+#       Main Function
+#------------------------
 def mainControl():
+    # status of back door, if front door is open check back door. If backdoor is open show X else show Check Mark. 
+    # For every 5 minutes collect the data {Temperature, Humidity, Front Door Status}
     while True:
         print("Test")
 
